78ae3d4cbb1dd898cb043b98b788cc1888108bcf
qb-athirau <62994344+qb-athirau@users.noreply.github.com>
2020-06-26 06:26:07 +0530

    commit build folder

---
+++ build/precache-manifest.84089f61ec62ba6ac44ef4b38467c921.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42391ef955a5fcb5736d9342aa0d6af3",
+    "url": "/project-management/index.html"
+  },
+  {
+    "revision": "d7238298e00cee150057",
+    "url": "/project-management/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "0903cdc34a58e30f7fe4",
+    "url": "/project-management/static/css/main.9d3d29de.chunk.css"
+  },
+  {
+    "revision": "d7238298e00cee150057",
+    "url": "/project-management/static/js/2.00554556.chunk.js"
+  },
+  {
+    "revision": "99e7db166412e59dfdcdb90fde83cb77",
+    "url": "/project-management/static/js/2.00554556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0edd42e37218b378f7a7",
+    "url": "/project-management/static/js/3.e064a62a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project-management/static/js/3.e064a62a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92a65bce8c0f457a565d",
+    "url": "/project-management/static/js/4.ac4e7ea8.chunk.js"
+  },
+  {
+    "revision": "31a7e5dc674a60eebb9d",
+    "url": "/project-management/static/js/5.09cd1e17.chunk.js"
+  },
+  {
+    "revision": "0903cdc34a58e30f7fe4",
+    "url": "/project-management/static/js/main.7217928a.chunk.js"
+  },
+  {
+    "revision": "d66765b22535ea1214fe",
+    "url": "/project-management/static/js/runtime-main.09e721f8.js"
+  },
+  {
+    "revision": "42cf9f0820d16f3ac2c26a7710ce70f2",
+    "url": "/project-management/static/media/Poppins-Black.42cf9f08.ttf"
+  },
+  {
+    "revision": "16dc9819cb371f042475ae8b0c309cba",
+    "url": "/project-management/static/media/Poppins-BlackItalic.16dc9819.ttf"
+  },
+  {
+    "revision": "c23534acbeddbaadfd0ab2d2bbfdfc84",
+    "url": "/project-management/static/media/Poppins-Bold.c23534ac.ttf"
+  },
+  {
+    "revision": "5bda2710554b202e3e69cf2ec4e0e663",
+    "url": "/project-management/static/media/Poppins-BoldItalic.5bda2710.ttf"
+  },
+  {
+    "revision": "6b78c7ec468eb0e13c6c5c4c39203986",
+    "url": "/project-management/static/media/Poppins-ExtraBold.6b78c7ec.ttf"
+  },
+  {
+    "revision": "e59d32640b0023d4c484239b189e1f01",
+    "url": "/project-management/static/media/Poppins-ExtraBoldItalic.e59d3264.ttf"
+  },
+  {
+    "revision": "2a47a29ceb33c966c8d79f8d5a5ea448",
+    "url": "/project-management/static/media/Poppins-Light.2a47a29c.ttf"
+  },
+  {
+    "revision": "926a872e20b6555f4d2a395d6d1bc01e",
+    "url": "/project-management/static/media/Poppins-LightItalic.926a872e.ttf"
+  },
+  {
+    "revision": "ba95810b56f476990ca71d15139d5111",
+    "url": "/project-management/static/media/Poppins-Medium.ba95810b.ttf"
+  },
+  {
+    "revision": "9e911c68b683bfce45bb6effc5403b1d",
+    "url": "/project-management/static/media/Poppins-MediumItalic.9e911c68.ttf"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/project-management/static/media/Poppins-Regular.41e8dead.ttf"
+  },
+  {
+    "revision": "342ba3d8ac29ac8c38d7cef8efbf2dc9",
+    "url": "/project-management/static/media/Poppins-SemiBold.342ba3d8.ttf"
+  },
+  {
+    "revision": "0fc816cbaaddf3d5b8c6b00c4acddf58",
+    "url": "/project-management/static/media/Poppins-SemiBoldItalic.0fc816cb.ttf"
+  },
+  {
+    "revision": "c0fafa8397437c95848724aed686d63b",
+    "url": "/project-management/static/media/Poppins-Thin.c0fafa83.ttf"
+  },
+  {
+    "revision": "272bb70478d0d2c7fc9b4d6c948b5f18",
+    "url": "/project-management/static/media/Poppins-ThinItalic.272bb704.ttf"
+  }
+]);
